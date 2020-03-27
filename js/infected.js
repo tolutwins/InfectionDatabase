60ebd5bbac79ed46a4ef1afc36f158410c773d88
tolutwins <57286829+tolutwins@users.noreply.github.com>
2020-03-27 13:20:04 +0100

    wip
    worked on js

--- js/infected.js
+++ js/infected.js
@@ -1,34 +1,56 @@
+//
 let patients = [];
 
 function getPatientsList() {
-
+    let patientsString = '';
+    for(let i=0; i<patients.length; i++) {
+        patientsString += `<tr><td>${i+1}</td><td>${patients[i].FullName}</td><td>${patients[i].Severity}</td></tr>`;
+    }
+    return patientsString;
 }
 
-function displayPatients() {
-
-}
 
 function getPatientsByName() {
 
 
 }
 
-function  displayPatients() {
-
-
+function  displayPatients(displayValue) {
+    $('#tbPatients').append(displayValue);
 }
 
 function createPatientObject() {
+    // grab values of fullname and severity
+    const fullName = $('#txtFullName').val();
+    const severity = $('#slSeverity option:selected').text();
+    
+    // build patient object
+    const patientObject = {
+        FullName: fullName,
+        Severity: severity
+    };
 
+    // update patients array
+    updatePatientsList(patientObject);
 
+    // load patient list
+    loadPage();
 }
 
-function updatePtientsList() {
+function updatePatientsList(patientObject) {
+    // add patient object to patients array
+    patients.push(patientObject);
+}
 
-
+function loadPage() {
+    const patientsString = getPatientsList();
+    displayPatients(patientsString);
 }
 
 $(document).ready(function() {
-    getPatientsList();
-    displayPatients();
+    loadPage();
+});
+
+$('#btnAdd').click(function() {
+    createPatientObject();
 });

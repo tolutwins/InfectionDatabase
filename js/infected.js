a1c550ceadbc2265a94c04fe3adbc4d150ac0bb4
tolutwins <57286829+tolutwins@users.noreply.github.com>
2020-03-27 11:52:22 +0100

    new files
    worked on html css and js

---
+++ js/infected.js
@@ -0,0 +1,34 @@
+let patients = [];
+
+function getPatientsList() {
+
+}
+
+function displayPatients() {
+
+}
+
+function getPatientsByName() {
+
+
+}
+
+function  displayPatients() {
+
+
+}
+
+function createPatientObject() {
+
+
+}
+
+function updatePtientsList() {
+
+
+}
+
+$(document).ready(function() {
+    getPatientsList();
+    displayPatients();
+});
